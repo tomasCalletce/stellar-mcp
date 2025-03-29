64a472941337ebab5a9f5ad60014aed3589ceabb
tomas <64665732+tomasCalletce@users.noreply.github.com>
2025-03-29 17:51:57 -0300

    feat: correct port

--- src/index.ts
+++ src/index.ts
@@ -153,4 +153,7 @@ app.post("/messages", async (req: Request, res: Response) => {
   }
 });
 
-app.listen(process.env.PORT || 4000);
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
